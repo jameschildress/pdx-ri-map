6e9482e83668f8a672455dbbc7b9a8c4efa16a90
James Childress <james.childress@me.com>
2013-12-31 13:00:04 -0800

    readying backbone app skeleton

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,5 +2,11 @@
 //= require vendor/underscore-min
 //= require vendor/backbone-min
 
-//= require app/app
+//= require app/config
+
+//= require_tree app/models
+//= require_tree app/collections
+//= require_tree app/views
+//= require_tree app/routers
+
 //= require app/initialize
