080ed666458f8cdd21da62bb19e15f27c3207537
James Childress <james.childress@me.com>
2014-01-02 16:57:54 -0800

    deploy script only pushes rackup file to server

--- source/rack_jsonp_proxy/deploy.sh
+++ source/rack_jsonp_proxy/deploy.sh
@@ -2,8 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-# Copy directory contents to remote host
-scp -r $DIR "${SSH_LINODE_USER}@${SSH_LINODE_HOST}:${PDX_RI_REMOTE_DIR}"
+# Copy rackup file to remote host
+scp "${DIR}/config.ru" "${SSH_LINODE_USER}@${SSH_LINODE_HOST}:${PDX_RI_REMOTE_DIR}/config.ru"
 
 # Restart Passenger
 ssh -l $SSH_LINODE_USER $SSH_LINODE_HOST "touch ${PDX_RI_REMOTE_DIR}tmp/restart.txt"
