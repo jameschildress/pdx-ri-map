4c8d8dd7731bb63b96b50506738317a3c7fa445a
James Childress <james.childress@me.com>
2014-01-03 09:49:26 -0800

    parse methods for models

--- source/javascripts/app/models/inspection.js
+++ source/javascripts/app/models/inspection.js
@@ -5,17 +5,32 @@
 
   App.Inspection = Backbone.Model.extend({
     
-    defaults: {
-      id:             0
-    , restaurantName: ''
-    , streetAddress:  ''
-    , city:           ''
-    , zipCode:        ''
-    , latitude:       0
-    , longitude:      0
-    , type:           ''
-    , date:           null
-    , score:          0
+    defaults: {         
+      id:               0
+    , restaurantName:   ''
+    , streetAddress:    ''
+    , city:             ''
+    , zipCode:          ''
+    , latitude:         0
+    , longitude:        0
+    , type:             ''
+    , date:             null
+    , score:            0
+    }
+    
+  , parse: function(response) {
+      return {
+        id:             parseInt(response.inspection_number)
+      , restaurantName: response.name.trim()
+      , streetAddress:  response.address.street.trim()
+      , city:           response.address.city.trim()
+      , zipCode:        response.address.zip.trim()
+      , latitude:       parseFloat(response.location.latitude)
+      , longitude:      parseFloat(response.location.longitude)
+      , type:           response.type.trim()
+      , date:           new Date(response.date)
+      , score:          parseInt(response.score)
+      };
     }
     
   });
@@ -23,3 +38,22 @@
   
   
 }());
+
+
+// name: "Acropolis Mcloughlin",
+// inspection_number: "9003667",
+// restaurant_id: "42",
+// type: "FoodSvcSemi",
+// date: "2012-10-14",
+// score: "90",
+// address: {
+//     street: "8325 SE Mc Loughlin Blvd",
+//     city: "Portland",
+//     zip: "97202"
+// },
+// location: {
+//     GISX: "7653323.41801",
+//     GISY: "662042.809488",
+//     Latitude: "45.462672740",
+//     Longitude: "-122.639146744"
+// }
--- source/javascripts/app/models/violation.js
+++ source/javascripts/app/models/violation.js
@@ -12,6 +12,15 @@
     , correction: ''
     }
     
+  , parse: function(response) {
+      return {
+        lawID:      response.law.trim()
+      , lawText:    response.violation_rule.trim()
+      , comment:    response.violation_comments.trim()
+      , correction: response.corrective_text.trim()
+      };
+    }
+    
   });
   
   
