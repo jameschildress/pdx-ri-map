7d59e6addfb764415456e6cde41bbb83b37f35d2
James Childress <james.childress@me.com>
2014-01-07 10:14:24 -0800

    wooo circle

--- source/javascripts/app/views/query_map_marker.js
+++ source/javascripts/app/views/query_map_marker.js
@@ -6,18 +6,25 @@
   App.QueryMapMarkerView = Backbone.View.extend({
   
     render: function(latLng) {
+      this.circle.setCenter(latLng);
+      this.circle.setMap(App.map);
       this.marker.setPosition(latLng);
       this.marker.setMap(App.map);
     }
     
   , initialize: function() {
       this.marker = new google.maps.Marker();
-      // this.circle = 
+      this.circle = new google.maps.Circle({
+        radius: App.config.proxy.maxDistance / 0.00062137  // convert miles to meters
+      , strokeOpacity: 0
+      , fillColor:     '#0077cc'
+      , fillOpacity:   0.4
+      });
     }
     
   , hide: function(){
       this.marker.setMap(null);
-      // this.circle.setMap(null);
+      this.circle.setMap(null);
     }
   
   });
