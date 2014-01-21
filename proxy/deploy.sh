9c43fa8ef93ea34b26692a83f7b1c84c4762792b
James Childress <james.childress@me.com>
2014-01-21 09:25:23 -0800

    updated env vars

--- proxy/deploy.sh
+++ proxy/deploy.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 # Copy rackup file to remote host
-scp "${DIR}/config.ru" "${SSH_LINODE_USER}@${SSH_LINODE_HOST}:${PDX_RI_REMOTE_DIR}/config.ru"
+scp "${DIR}/config.ru" "${SSH_LINODE_USER}@${SSH_LINODE_HOST}:${PDX_RI_PROXY_REMOTE_DIR}/config.ru"
 
 # Restart Passenger
-ssh -l $SSH_LINODE_USER $SSH_LINODE_HOST "touch ${PDX_RI_REMOTE_DIR}tmp/restart.txt"
+ssh -l $SSH_LINODE_USER $SSH_LINODE_HOST "touch ${PDX_RI_PROXY_REMOTE_DIR}tmp/restart.txt"
